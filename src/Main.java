ea422e0962d621a8fffc20dd21b069b62473c572
Ryan McDougall <rp.mcdougall@gmail.com>
2019-01-06 13:08:26 -0600

    Bubble Sort

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+
+import com.rm.datastructures.Arrays.BubbleSort;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
+        //Arrays Seed Data
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+        System.out.println("=====Seed Data======");
+        for(int i : intArray){
+            System.out.print(i+" ");
+        }
+        System.out.println(" ");
+        //Array Sorting
+        System.out.println("=====BubbleSort=====");
+        BubbleSort.sort(intArray);
+        System.out.println("====================");
+
+
     }
 }
---
+++ src/com/rm/datastructures/Arrays/BubbleSort.java
@@ -0,0 +1,29 @@
+package com.rm.datastructures.Arrays;
+
+public class BubbleSort {
+
+    public static int[] sort(int[] arr) {
+
+        for(int lastUnsortedIndex = arr.length -1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            for(int i = 0; i < lastUnsortedIndex; i++) {
+                if(arr[i] > arr[i+1]) {
+                    swap(arr, i, i+1);
+                }
+            }
+        }
+
+        for(int i: arr ){
+            System.out.println(i);
+        }
+
+        return arr;
+    }
+
+     private static void swap(int[] arr, int i, int j) {
+        if (i == j) {return;}
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+}
